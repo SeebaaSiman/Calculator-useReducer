20586316e0f247ccd124e6e3c569b360a8149b45
Sebastian <seebaasiman@gmail.com>
2023-07-14 11:46:44 -0300

    Footer agregado

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyles from "./style/GlobalStyles";
 import { Calculator } from "./components/Calculator";
+import { Footer } from "./components/footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyles />
     <Calculator />
+    <Footer />
   </React.StrictMode>
 );
